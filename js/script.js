113186d24138729efd68f33240796bb7ad8f253c
Arwa <arwamsharif@gmail.com>
2023-03-21 18:16:52 -0500

    refined version of last commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-
-let objectsDiv = document.querySelector(".btns")
+let objectsDiv = document.querySelector(".btns");
 // console.dir(objectsDiv);
 
-let eggHid = objectsDiv.hasAttribute("eggs-hidden") 
+let eggHid = objectsDiv.hasAttribute("eggs-hidden");
 
-let objectsBtns = objectsDiv.querySelectorAll("button")
+let objectsBtns = objectsDiv.querySelectorAll("button");
 
-let messageBox = document.querySelector(".message")
+let messageBox = document.querySelector(".message");
 
-objectsBtns.forEach(objBtn => {
-    objBtn.addEventListener("click", () =>{
-        console.log(messageBox.textContent = `You found the ${objBtn.textContent}!`)
-    })
-})
+objectsBtns.forEach((objBtn) => {
+  objBtn.addEventListener("click", () => {
+    console.log(
+      (messageBox.textContent = `You found the ${objBtn.textContent}!`)
+    );
+  });
+});
