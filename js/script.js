f6dfde353e7e234d45f03702eafd13860e89f973
Arwa <arwamsharif@gmail.com>
2023-03-21 18:15:14 -0500

    30% progress: set up the game img and placed and tested buttons functionality except for spider

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+let objectsDiv = document.querySelector(".btns")
+// console.dir(objectsDiv);
+
+let eggHid = objectsDiv.hasAttribute("eggs-hidden") 
+
+let objectsBtns = objectsDiv.querySelectorAll("button")
+
+let messageBox = document.querySelector(".message")
+
+objectsBtns.forEach(objBtn => {
+    objBtn.addEventListener("click", () =>{
+        console.log(messageBox.textContent = `You found the ${objBtn.textContent}!`)
+    })
+})
