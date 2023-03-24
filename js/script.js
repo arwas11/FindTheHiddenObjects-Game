e7fa2d89eb247c445f7570afabc04cf49667d6bb
Arwa <arwamsharif@gmail.com>
2023-03-24 13:03:54 -0500

    game is functioning the way it should

--- js/script.js
+++ js/script.js
@@ -2,107 +2,279 @@ let hiddenDiv = document.querySelector(".btns-hidden");
 
 let hiddenBtns = hiddenDiv.querySelectorAll("button");
 
-let foundDiv = document.querySelector(".divs-found");
+let foundDiv = document.querySelector(".hidden-obj");
 
 let foundDivs = foundDiv.querySelectorAll("div");
 
 let messageBtn = document.querySelector(".message-btn");
 
-let endMessage = document.querySelector(".end-content")
+let endMessage = document.querySelector(".end-slide")
 
-let currentObj = 11
+let timer = document.querySelector("#time")
 
-hiddenBtns.forEach((objBtn) => { 
-countdown(5);
-objBtn.addEventListener(
+let winLose = document.querySelector("#win-lose")
+
+let foundAll = document.querySelector("#if-found-all")
+
+let clicked = [11, 12];
+
+for( let objBtn of hiddenBtns){ 
+  countdown(1);
+
+   objBtn.addEventListener(
     "click",
     () => {
-      //make a switch statement
-      if (objBtn.textContent === "Eggs") {
+      //make it a switch statement
+       if (objBtn.textContent === "Eggs"){
+        clicked.push(1)
+        console.log(clicked.length)
         document.querySelector("#eggs-found.cross-eggs").style.color = "black";
         timedMessage(objBtn.textContent);
-        currentObj ++
+        
+        if (clicked.length === 12){
+          console.log(clicked.length)
+          winLose.innerText = "Nice Job!"
+          console.log(winLose);
+          foundAll.innerText = "You found all of the 10 hidden objects."
+          console.log(foundAll);
+          endMessage.style.display = "block"
+        } 
+        return
       }
-      if (objBtn.textContent === "Hedgehog") {
-        document.querySelector("#hedgehog-found.cross-hedgehog").style.color =
-          "black";
-        timedMessage(objBtn.textContent);
-        return;
-      }
-      if (objBtn.textContent === "Cricket") {
-        document.querySelector("#cricket-found.cross-cricket").style.color =
-          "black";
-        timedMessage(objBtn.textContent);
-        return;
-      }
-      if (objBtn.textContent === "Flower") {
-        document.querySelector("#flower-found.cross-flower").style.color =
-          "black";
-        timedMessage(objBtn.textContent);
-        return;
-      }
-      if (objBtn.textContent === "Dragonfly") {
-        document.querySelector("#dragonfly-found.cross-dragonfly").style.color =
-          "black";
-        timedMessage(objBtn.textContent);
-        return;
-      }
-      if (objBtn.textContent === "Fish") {
-        document.querySelector("#fish-found.cross-fish").style.color = "black";
-        timedMessage(objBtn.textContent);
-        return;
-      }
-      if (objBtn.textContent === "Spider") {
-        document.querySelector("#spider-found.cross-spider").style.color =
-          "black";
-        timedMessage(objBtn.textContent);
-        return;
-      }
-      if (objBtn.textContent === "Worm") {
-        document.querySelector("#worm-found.cross-worm").style.color = "black";
-        timedMessage(objBtn.textContent);
-        return;
-      }
-      if (objBtn.textContent === "Bird") {
-        document.querySelector("#bird-found.cross-bird").style.color = "black";
-        timedMessage(objBtn.textContent);
-        return;
-      }
-      if (objBtn.textContent === "Grasshopper") {
-        document.querySelector(
-          "#grasshopper-found.cross-grasshopper"
-        ).style.color = "black";
-        timedMessage(objBtn.textContent);
-        return;
-
-      }
-      winLose()
     },
-    { once: true }
-    
+    { once: true }  
   );
-   
- 
+    
+objBtn.addEventListener(
+  "click",
+  () => {
+    if (objBtn.textContent === "Hedgehog") {
+      clicked.push(2)
+      console.log(clicked.length)
+      document.querySelector("#hedgehog-found.cross-hedgehog").style.color =
+        "black";
+      timedMessage(objBtn.textContent);
+      
+      if (clicked.length === 12){
+        console.log(clicked.length)
+        winLose.innerText = "Nice Job!"
+        console.log(winLose);
+        foundAll.innerText = "You found all of the 10 hidden objects."
+        console.log(foundAll);
+        endMessage.style.display = "block"
+      } 
+      return;
+    }
+  },
+  { once: true }  
+);
+objBtn.addEventListener(
+  "click",
+  () => {
+    if (objBtn.textContent === "Cricket") {
+      clicked.push(3)
+      console.log(clicked.length)
+      document.querySelector("#cricket-found.cross-cricket").style.color =
+        "black";
+      timedMessage(objBtn.textContent);
+      
+      if (clicked.length === 12){
+        console.log(clicked.length)
+        winLose.innerText = "Nice Job!"
+        console.log(winLose);
+        foundAll.innerText = "You found all of the 10 hidden objects."
+        console.log(foundAll);
+        endMessage.style.display = "block"
+      } 
+      return;
+    }
+  },
+  { once: true }  
+);
 
-});
+objBtn.addEventListener(
+  "click",
+  () => {
+    if (objBtn.textContent === "Flower") {
+      clicked.push(4)
+      console.log(clicked.length)
+      document.querySelector("#flower-found.cross-flower").style.color =
+        "black";
+      timedMessage(objBtn.textContent);
+      
+      if (clicked.length === 12){
+        console.log(clicked.length)
+        winLose.innerText = "Nice Job!"
+        console.log(winLose);
+        foundAll.innerText = "You found all of the 10 hidden objects."
+        console.log(foundAll);
+        endMessage.style.display = "block"
+      } 
+      return;
+    }
+  },
+  { once: true }  
+);
+
+objBtn.addEventListener(
+  "click",
+  () => {
+    if (objBtn.textContent === "Dragonfly") {
+      clicked.push(5)
+      console.log(clicked.length)
+      document.querySelector("#dragonfly-found.cross-dragonfly").style.color =
+        "black";
+      timedMessage(objBtn.textContent); 
+      
+      if (clicked.length === 12){
+        console.log(clicked.length)
+        winLose.innerText = "Nice Job!"
+        console.log(winLose);
+        foundAll.innerText = "You found all of the 10 hidden objects."
+        console.log(foundAll);
+        endMessage.style.display = "block"
+      } 
+      return;
+    }
+  },
+  { once: true }  
+);
+
+objBtn.addEventListener(
+  "click",
+  () => {
+    if (objBtn.textContent === "Fish") {
+      clicked.push(6)
+      console.log(clicked.length)
+      document.querySelector("#fish-found.cross-fish").style.color = "black";
+      timedMessage(objBtn.textContent);
+      
+      if (clicked.length === 12){
+        console.log(clicked.length)
+        winLose.innerText = "Nice Job!"
+        console.log(winLose);
+        foundAll.innerText = "You found all of the 10 hidden objects."
+        console.log(foundAll);
+        endMessage.style.display = "block"
+      } 
+      return;
+    }
+  },
+  { once: true }  
+);
+
+objBtn.addEventListener(
+  "click",
+  () => {
+    if (objBtn.textContent === "Spider") {
+      clicked.push(7)
+      console.log(clicked.length)
+      document.querySelector("#spider-found.cross-spider").style.color =
+        "black";
+      timedMessage(objBtn.textContent);
+      
+      if (clicked.length === 12){
+        console.log(clicked.length)
+        winLose.innerText = "Nice Job!"
+        console.log(winLose);
+        foundAll.innerText = "You found all of the 10 hidden objects."
+        console.log(foundAll);
+        endMessage.style.display = "block"
+      } 
+      return;
+    }
+  },
+  { once: true }  
+);
+
+objBtn.addEventListener(
+  "click",
+  () => {
+    if (objBtn.textContent === "Worm") {
+      clicked.push(8)
+      console.log(clicked.length)
+      document.querySelector("#worm-found.cross-worm").style.color = "black";
+      timedMessage(objBtn.textContent);
+      
+      if (clicked.length === 12){
+        console.log(clicked.length)
+        winLose.innerText = "Nice Job!"
+        console.log(winLose);
+        foundAll.innerText = "You found all of the 10 hidden objects."
+        console.log(foundAll);
+        endMessage.style.display = "block"
+      } 
+      return;
+    }
+  },
+  { once: true }  
+);
+
+objBtn.addEventListener(
+  "click",
+  () => {
+    if (objBtn.textContent === "Bird") {
+      clicked.push(9)
+      console.log(clicked.length)
+      document.querySelector("#bird-found.cross-bird").style.color = "black";
+      timedMessage(objBtn.textContent);
+      
+      if (clicked.length === 12){
+        console.log(clicked.length)
+        winLose.innerText = "Nice Job!"
+        console.log(winLose);
+        foundAll.innerText = "You found all of the 10 hidden objects."
+        console.log(foundAll);
+        endMessage.style.display = "block"
+      }
+      return;
+    }
+  },
+  { once: true }  
+);
+
+objBtn.addEventListener(
+  "click",
+  () => {
+    if (objBtn.textContent === "Grasshopper") {
+      clicked.push(10)
+      console.log(clicked.length)
+      document.querySelector(
+        "#grasshopper-found.cross-grasshopper"
+      ).style.color = "black";
+      timedMessage(objBtn.textContent);
+
+      if (clicked.length === 12){
+        console.log(clicked.length)
+        winLose.innerText = "Nice Job!"
+        console.log(winLose);
+        foundAll.innerText = "You found all of the 10 hidden objects."
+        console.log(foundAll);
+        endMessage.style.display = "block"
+      } 
+      return;
+    }
+  },
+  { once: true }  
+);
+
+};
 
 
 function timedMessage(textContent) {
-  messageBtn.style.display = "block"
-  let foundMsg =
-    (messageBtn.textContent = `
-       You 
-      Found 
-       The 
-      Hidden 
-    ${textContent}!`);
-  console.log(foundMsg);
-  setTimeout(() => {
-    //   objBtn.removeEventListener('click', ())
-    messageBtn.style.display = "none";
-    return;
-  }, 5000);
-
+messageBtn.style.display = "block"
+let foundMsg =
+  (messageBtn.textContent = `
+     You 
+    Found 
+     The 
+    Hidden 
+  ${textContent}!`);
+console.log(foundMsg);
+setTimeout(() => {
+  messageBtn.style.display = "none";
+  return;
+}, 5000);
 }
 
 
@@ -121,27 +293,11 @@ function countdown(minutes) {
               countdown(mins-1);           
           }
       }
+      if (timer.textContent === "0:00"){
+        winLose.textContent = "Time's Up!"
+        foundAll.innerText = "You couldn't find all of the hidden objects."
+        endMessage.style.display = "block"
+        }
   }
   tick();
 }
-
-countdown(5); 
-if (countdown === "0:00"){
-  endMessage.style.display = "block"
-} else {
-
-}
-// currentTotal <= hiddenBtns.length + 1
-// function winLose(){
-//   if (currentTotal <= hiddenBtns.length){
-//     document.getElementById("win-lose").innerText = "Nice Job!"
-//     document.getElementById("#if-found-all").innerText = "You found all of the 10 hidden objects."
-//     endMessage.style.display = "block"
-//   } 
-  
-//   if (document.getElementById("#time").textContent === "0:00"){
-//     document.getElementById("win-lose").innerText = "Time's Up!"
-//     document.getElementById("#if-found-all").innerText = "______"
-//     endMessage.style.display = "block"
-//   }
-// }
