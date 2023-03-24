cc6add0c31ae23595a750217987b976bf45fa7ea
Arwa <arwamsharif@gmail.com>
2023-03-23 23:55:47 -0500

    90% progress

--- js/script.js
+++ js/script.js
@@ -1,123 +1,147 @@
 let hiddenDiv = document.querySelector(".btns-hidden");
-// console.dir(hiddenDiv);
-
-// let eggHid = hiddenDiv.hasAttribute("eggs-hidden");
 
 let hiddenBtns = hiddenDiv.querySelectorAll("button");
 
-let foundDiv = document.querySelector(".divs-found")
+let foundDiv = document.querySelector(".divs-found");
 
-let foundDivs = foundDiv.querySelectorAll('div')
-// console.dir(foundDivs)
-
-
-// let crossEggs = foundDiv.querySelector(".cross-eggs")
-
-// foundDivs.style.display = "none";
-
+let foundDivs = foundDiv.querySelectorAll("div");
 
 let messageBtn = document.querySelector(".message-btn");
 
+let endMessage = document.querySelector(".end-content")
 
+let currentObj = 11
 
-
-
-hiddenBtns.forEach((objBtn) => {
-  objBtn.addEventListener(
+hiddenBtns.forEach((objBtn) => { 
+countdown(5);
+objBtn.addEventListener(
     "click",
     () => {
-      if (objBtn.textContent === "eggs"){
+      //make a switch statement
+      if (objBtn.textContent === "Eggs") {
         document.querySelector("#eggs-found.cross-eggs").style.color = "black";
-        console.log(
-          (messageBtn.textContent = `You found the hidden ${objBtn.textContent}!`)
-        );
-        return
-      } else if (objBtn.textContent === "hedgehog") {
-        document.querySelector("#hedgehog-found.cross-hedgehog").style.color = "black"
-        console.log(
-          (messageBtn.textContent = `You found the hidden ${objBtn.textContent}!`)
-        );
-        return
-      } else if (objBtn.textContent === "cricket") {
-        document.querySelector("#cricket-found.cross-cricket").style.color = "black"
-        console.log(
-          (messageBtn.textContent = `You found the hidden ${objBtn.textContent}!`)
-        );
-        return
-      } else if (objBtn.textContent === "flower") {
-        document.querySelector("#flower-found.cross-flower").style.color = "black"
-        console.log(
-          (messageBtn.textContent = `You found the hidden ${objBtn.textContent}!`)
-        );
-        return
-      } else if (objBtn.textContent === "dragonfly") {
-        document.querySelector("#dragonfly-found.cross-dragonfly").style.color = "black"
-        console.log(
-          (messageBtn.textContent = `You found the hidden ${objBtn.textContent}!`)
-        );
-        return
-      } else if (objBtn.textContent === "fish") {
-        document.querySelector("#fish-found.cross-fish").style.color = "black"
-        console.log(
-          (messageBtn.textContent = `You found the hidden ${objBtn.textContent}!`)
-        );
-        return
-      } else if (objBtn.textContent === "spider") {
-        document.querySelector("#spider-found.cross-spider").style.color = "black"
-        console.log(
-          (messageBtn.textContent = `You found the hidden ${objBtn.textContent}!`)
-        );
-        return
-      } else if (objBtn.textContent === "worm") {
-        document.querySelector("#worm-found.cross-worm").style.color = "black"
-        console.log(
-          (messageBtn.textContent = `You found the hidden ${objBtn.textContent}!`)
-        );
-        return
-      } else if (objBtn.textContent === "bird") {
-        document.querySelector("#bird-found.cross-bird").style.color = "black"
-        console.log(
-          (messageBtn.textContent = `You found the hidden ${objBtn.textContent}!`)
-        );
-        return
-      } else if (objBtn.textContent === "grasshopper") {
-        console.log(
-          (messageBtn.textContent = `You found the hidden ${objBtn.textContent}!`)
-        );
-        document.querySelector("#grasshopper-found.cross-grasshopper").style.color = "black"
-        return
+        timedMessage(objBtn.textContent);
+        currentObj ++
       }
+      if (objBtn.textContent === "Hedgehog") {
+        document.querySelector("#hedgehog-found.cross-hedgehog").style.color =
+          "black";
+        timedMessage(objBtn.textContent);
+        return;
+      }
+      if (objBtn.textContent === "Cricket") {
+        document.querySelector("#cricket-found.cross-cricket").style.color =
+          "black";
+        timedMessage(objBtn.textContent);
+        return;
+      }
+      if (objBtn.textContent === "Flower") {
+        document.querySelector("#flower-found.cross-flower").style.color =
+          "black";
+        timedMessage(objBtn.textContent);
+        return;
+      }
+      if (objBtn.textContent === "Dragonfly") {
+        document.querySelector("#dragonfly-found.cross-dragonfly").style.color =
+          "black";
+        timedMessage(objBtn.textContent);
+        return;
+      }
+      if (objBtn.textContent === "Fish") {
+        document.querySelector("#fish-found.cross-fish").style.color = "black";
+        timedMessage(objBtn.textContent);
+        return;
+      }
+      if (objBtn.textContent === "Spider") {
+        document.querySelector("#spider-found.cross-spider").style.color =
+          "black";
+        timedMessage(objBtn.textContent);
+        return;
+      }
+      if (objBtn.textContent === "Worm") {
+        document.querySelector("#worm-found.cross-worm").style.color = "black";
+        timedMessage(objBtn.textContent);
+        return;
+      }
+      if (objBtn.textContent === "Bird") {
+        document.querySelector("#bird-found.cross-bird").style.color = "black";
+        timedMessage(objBtn.textContent);
+        return;
+      }
+      if (objBtn.textContent === "Grasshopper") {
+        document.querySelector(
+          "#grasshopper-found.cross-grasshopper"
+        ).style.color = "black";
+        timedMessage(objBtn.textContent);
+        return;
 
-      // setTimeout(() =>{
-      //   objBtn.removeEventListener('click', ())
-      // } )
-      messageBtn.addEventListener("click", (e) => {
-        messageBtn.style.color = "black";
-      });
-
-      
-      //   return
+      }
+      winLose()
     },
     { once: true }
+    
   );
-
+   
+ 
 
 });
 
-let currentObjBtnIndex = 0;
 
-// whilt hiddenBtns.length < currentObjBtnIndex){
-//     console.log(currentObjBtnIndex);
-//     // currentObjBtnIndex.addEventListener("click", () => {
-//     //     console.log(
-//     //       (messageBtn.textContent = `You found the ${objBtn.textContent}!`)
-//     //     );
-//     //      messageBtn.addEventListener('click', e =>{
-//     //         messageBtn.style.display = "none";
-//     //         foundObj t hiddenBtns.pop()
-//     //         console.log(foundObj);
-//     //       })
+function timedMessage(textContent) {
+  messageBtn.style.display = "block"
+  let foundMsg =
+    (messageBtn.textContent = `
+       You 
+      Found 
+       The 
+      Hidden 
+    ${textContent}!`);
+  console.log(foundMsg);
+  setTimeout(() => {
+    //   objBtn.removeEventListener('click', ())
+    messageBtn.style.display = "none";
+    return;
+  }, 5000);
 
-//     //       return
-//     //   });
+}
+
+
+function countdown(minutes) {
+  var seconds = 60;
+  var mins = minutes
+  function tick() {
+      var time = document.querySelector("#time");
+      var current_minutes = mins-1
+      seconds--;
+      time.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+      if( seconds > 0 ) {
+          setTimeout(tick, 1000);
+      } else {
+          if(mins > 1){
+              countdown(mins-1);           
+          }
+      }
+  }
+  tick();
+}
+
+countdown(5); 
+if (countdown === "0:00"){
+  endMessage.style.display = "block"
+} else {
+
+}
+// currentTotal <= hiddenBtns.length + 1
+// function winLose(){
+//   if (currentTotal <= hiddenBtns.length){
+//     document.getElementById("win-lose").innerText = "Nice Job!"
+//     document.getElementById("#if-found-all").innerText = "You found all of the 10 hidden objects."
+//     endMessage.style.display = "block"
+//   } 
+  
+//   if (document.getElementById("#time").textContent === "0:00"){
+//     document.getElementById("win-lose").innerText = "Time's Up!"
+//     document.getElementById("#if-found-all").innerText = "______"
+//     endMessage.style.display = "block"
+//   }
 // }
